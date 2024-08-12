e7c16a347802a915ee2d6986221f01462b235f78
Noiseneks <noiseneks@gmail.com>
2024-08-12 21:57:08 +0300

    Update test

--- src/test/java/com/github/Noiseneks/taskManagementSpring/RequestUnitTest.java
+++ src/test/java/com/github/Noiseneks/taskManagementSpring/RequestUnitTest.java
@@ -6,9 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,9 +28,9 @@ public class RequestUnitTest {
 
         String jsonExample = "{\"status\":\"UP\"}";
 
-        MvcResult mvcResult = mvc.perform(requestBuilder).andReturn();
-
-        throw new RuntimeException(mvcResult.getResponse().getContentAsString());
+        mvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json(jsonExample));
     }
 
 }
